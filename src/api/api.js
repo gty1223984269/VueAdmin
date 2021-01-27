e4ca7e191c07e1302e488ba7870f0b9ce86cff4e
unknown <1223984269@qq.com>
2021-01-27 12:25:51 +0800

    axios request with token

--- src/api/api.js
+++ src/api/api.js
@@ -5,37 +5,11 @@ let token = localStorage.getItem('token');
 // create an axios instance
 const service = axios.create({
   baseURL: '/api', // api的base_url
-  timeout: 5000 // request timeout
+  timeout: 5000, // request timeout
+  headers:{'x-nideshop-token':token}
 });
 
-// // request interceptor
-// service.interceptors.request.use(config => {
-//   // Do something before request is sent
-//   if (store.getters.token) {
-//     config.headers['X-Token'] = getToken() // 让每个请求携带token-- ['X-Token']为自定义key 请根据实际情况自行修改
-//   }
-//   return config
-// }, error => {
-//   // Do something with request error
-//   console.log(error) // for debug
-//   Promise.reject(error)
-// })
-
-// respone interceptor
-service.interceptors.response.use(
-  response => response,
-  error => {
-    console.log('err' + error); // for debug
-    Message({
-      message: error.message,
-      type: 'error',
-      duration: 5 * 1000
-    });
-    return Promise.reject(error);
-  });
-
 export const getSysMenuList = params => {
-  alert(token);
   return service.post(`${ip}/admin/backEnd/menuList`, params).then(res => res.data);
 };
 
